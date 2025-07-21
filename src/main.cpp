b1395857d401d421de47f37ecc3f49c487517d1f
Daniel Cil <daniel@d-cil.de>
2025-07-21 13:01:52 +0200

    add app and executeable icon

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+    app.setWindowIcon(QIcon(":/icons/snake_icon.png"));
     GamePanel game;
     game.show();
     return app.exec();
